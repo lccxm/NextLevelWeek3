31a14de9c29ddfc82ed0a3e3a2d0d6dc0db1e5b7
lccxm <luccamolon@gmail.com>
2020-10-14 01:02:36 -0300

    + cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,10 @@ import routes from './routes';
 import path from 'path';
 import 'express-async-errors';
 import errorHandler from './errors/handler';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
